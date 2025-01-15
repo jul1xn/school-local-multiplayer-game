c5b0a2355ac30e6430c2f58545b02c52430d84a9
Jul1xn <88789943+jul15xn@users.noreply.github.com>
2025-01-15 13:35:22 +0100

    Player 2 afgemaakt en extra feature toegevoegd
    Alleen player 2 kan moven tijden crouchen en player 1 niet, die staat stil. Hij kan daarmee alleen super jumpen

--- local mp game/Assets/Scripts/PlayerController.cs
+++ local mp game/Assets/Scripts/PlayerController.cs
@@ -10,12 +10,19 @@ public class PlayerController : MonoBehaviour
     public float crouchSpeedMultiplier = 0.35f;
     public float sprintSpeedMultiplier = 1.5f;
     public float groundCheckRadius = 0.1f;
+    public string groundTag = "Ground";
     [Header("Super jump properties")]
     public float crouchHoldTime = 1.5f;
     public float superJumpForce = 15f;
     private float jumpStarttime;
     private bool ischarging;
+    [Header("Extra crouch properties")]
+    public float normalSizeX = 0.6f;
+    public float normalSizeY = 1f;
+    public float crouchedSizeX = 0.6f;
+    public float crouchedSizeY = 0.2f;
     [Header("Player 1")]
+    public BoxCollider2D player1Collider;
     public Rigidbody2D player1Rb;
     public Animator player1Animator;
     public Transform p1GroundCheck;
@@ -23,6 +30,15 @@ public class PlayerController : MonoBehaviour
     private bool p1Crouching;
     private bool p1Sprinting;
     private bool p1SuperJump;
+    [Header("Player 2")]
+    public BoxCollider2D player2Collider;
+    public Rigidbody2D player2Rb;
+    public Animator player2Animator;
+    public Transform p2GroundCheck;
+    public SpriteRenderer player2SpriteRenderer;
+    private bool p2Crouching;
+    private bool p2Sprinting;
+    private bool p2SuperJump;
 
     private void Start()
     {
@@ -32,10 +48,10 @@ public class PlayerController : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(p1GroundCheck.position, groundCheckRadius);
-        //Gizmos.DrawWireSphere(p1GroundCheck.position, groundCheckRadius);
+        Gizmos.DrawWireSphere(p2GroundCheck.position, groundCheckRadius);
     }
 
-    private void Player1Logic()
+    private void P1Logic()
     {
         // Initialize values
         float horizontalValue = 0;
@@ -51,8 +67,17 @@ public class PlayerController : MonoBehaviour
         p1Crouching = Input.GetKey(KeyCode.S);
         p1Sprinting = Input.GetKey(KeyCode.LeftShift);
 
+        if (p1Crouching)
+        {
+            player1Collider.size = new Vector2(crouchedSizeX, crouchedSizeY);
+        }
+        else
+        {
+            player1Collider.size = new Vector2(normalSizeX, normalSizeY);
+        }
+
         // Set the speed
-        if (p1Crouching) { currentSpeed *= crouchSpeedMultiplier; }
+        if (p1Crouching) { currentSpeed = 0; }
         if (p1Sprinting && !p1Crouching) { currentSpeed *= sprintSpeedMultiplier; }
 
         // Apply the speed
@@ -98,13 +123,79 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void P2Logic()
+    {
+        // Initialize values
+        float horizontalValue = 0;
+        float currentSpeed = speed;
+        bool isGrounded = IsGrounded(p2GroundCheck);
+
+        // Get horizontal movement
+        if (Input.GetKey(KeyCode.LeftArrow)) { horizontalValue = -1; }
+        if (Input.GetKey(KeyCode.RightArrow)) { horizontalValue = 1; }
+        if (horizontalValue != 0) { player2SpriteRenderer.flipX = Input.GetKey(KeyCode.LeftArrow); }
+
+        // Set some variables
+        p2Crouching = Input.GetKey(KeyCode.DownArrow);
+
+        if (p2Crouching)
+        {
+            player2Collider.size = new Vector2(crouchedSizeX, crouchedSizeY);
+        }
+        else
+        {
+            player2Collider.size = new Vector2(normalSizeX, normalSizeY);
+        }
+
+        p2Sprinting = Input.GetKey(KeyCode.RightShift);
+
+        // Set the speed
+        if (p2Crouching) { currentSpeed *= crouchSpeedMultiplier; }
+        if (p2Sprinting && !p2Crouching) { currentSpeed *= sprintSpeedMultiplier; }
+
+        // Apply the speed
+        Vector2 targetVelocity = new(horizontalValue * currentSpeed * 100 * Time.deltaTime, player2Rb.velocity.y);
+        player2Rb.velocity = targetVelocity;
+
+        // Checks for that the player falls slower that it moves up
+        if (player2Rb.velocity.y < 0)
+        {
+            player2Rb.velocity += 2f * Physics2D.gravity.y * Time.deltaTime * Vector2.up; // Faster jump
+        }
+        else if (player2Rb.velocity.y > 0 && !Input.GetKey(KeyCode.UpArrow))
+        {
+            player2Rb.velocity += 1f * Physics2D.gravity.y * Time.deltaTime * Vector2.up; // Slower fall
+        }
+
+        // Check if player can jump
+        if (Input.GetKey(KeyCode.UpArrow) && isGrounded)
+        {
+            float force = jumpForce;
+            player2Rb.velocity = new Vector2(player2Rb.velocity.x, force);
+        }
+    }
+
     private bool IsGrounded(Transform groundCheck)
     {
-        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius) != null;
+        ContactFilter2D filter = new ContactFilter2D();
+        Collider2D[] results = new Collider2D[10];
+
+        Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, filter, results);
+
+        foreach (var collider in results)
+        {
+            if (collider != null && collider.CompareTag(groundTag))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void Update()
     {
-        Player1Logic();
+        P1Logic();
+        P2Logic();
     }
 }
